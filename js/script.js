f7d110008fde9ac4a2af3883a3f17049ea47454a
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2021-11-22 23:26:33 +0100

    add function for click event and see if clicked letter is correct or not, woin or lose, update hangman image

--- js/script.js
+++ js/script.js
@@ -47,8 +47,50 @@ function generateKeyButtons() {
   }
 }
 
-function handleGuess() {
+function resetValues() {
   console.log("test");
+}
+
+function handleGuess(event) {
+  const btn = document.getElementById(event.target.id);
+  btn.removeEventListener("click", handleGuess);
+  btn.classList.add("clicked");
+
+  // If guessed letter is correct it pushes the letter in correctLetters and runs displayBoard.
+  if (randomWord.includes(event.target.id)) {
+    correctLetters.push(event.target.id);
+    displayBoard();
+
+    // Then see if player have won
+    const test = wordEl.innerText.replaceAll("\n", "");
+    if (test.split("").length === randomWord.length) {
+      popupContainer.style.display = "flex";
+      popup.style.backgroundColor = "green";
+      finalMessage.textContent = `You won! Secret word: ${randomWord.join("")}`;
+      document
+        .getElementById("play-again")
+        .addEventListener("click", resetValues);
+    }
+  }
+  // If guessed letter is not correct it pushes the letter in wrongLetters, updates guesses and updates the hangman picture.
+  else {
+    wrongLetters.push(event.target.id);
+    document.getElementById(
+      "hangman-picture"
+    ).src = `images/h${wrongLetters.length}.png`;
+
+    // Then see if player have lost
+    if (wrongLetters.length === lifes) {
+      popupContainer.style.display = "flex";
+      popup.style.backgroundColor = "red";
+      finalMessage.textContent = `You lost! Secret word: ${randomWord.join(
+        ""
+      )}`;
+      document
+        .getElementById("play-again")
+        .addEventListener("click", resetValues);
+    }
+  }
 }
 
 function displayBoard() {
